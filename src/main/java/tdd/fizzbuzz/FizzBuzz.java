f51a2b87a57e480cf97bf175ab6c5d68ae1c1407
Marie Chow <chowma3@oocl.com>
2022-12-02 14:46:29 +0800

    feat: implement should_say_buzz_when_count_off_given_multiple_of_5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,13 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String countOff(int order) {
         if (order % 3 == 0) {
             return FIZZ;
+        } else if (order % 5 == 0) {
+            return BUZZ;
         }
         return Integer.toString(order);
     }
