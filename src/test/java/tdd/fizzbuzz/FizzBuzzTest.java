c429a2736c96ff390e018b73749106890e1ae80d
Marie Chow <chowma3@oocl.com>
2022-12-02 14:57:39 +0800

    test: add should_say_FizzBuzz_when_count_off_given_multiple_of_105

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -50,4 +50,13 @@ public class FizzBuzzTest {
         // then
         assertEquals("FizzBuzz", actual);
     }
+    @Test
+    void should_say_FizzBuzz_when_count_off_given_multiple_of_105() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // when
+        String actual = fizzBuzz.countOff(105);
+        // then
+        assertEquals("FizzBuzzWhizz", actual);
+    }
 }
