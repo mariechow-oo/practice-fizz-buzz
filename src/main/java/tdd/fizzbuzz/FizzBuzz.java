92a4cd7d615b20a38ee8ab0841ced9e5522937b1
Marie Chow <chowma3@oocl.com>
2022-12-02 14:25:37 +0800

    refactor: extract string as constant

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,12 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+
     public String countOff(int order) {
         if (order % 3 == 0) {
-            return "Fizz";
+            return FIZZ;
         }
         return Integer.toString(order);
     }
