4c01c3b1aa198b0b12b42825d90ac3b9871015d2
Marie Chow <chowma3@oocl.com>
2022-12-02 14:57:26 +0800

    feat: implement should_say_FizzBuzz_when_count_off_given_multiple_of_15

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -7,13 +7,16 @@ public class FizzBuzz {
     public static final String WHIZZ = "Whizz";
 
     public String countOff(int order) {
+        String countOffString = "";
         if (order % 3 == 0) {
-            return FIZZ;
-        } else if (order % 5 == 0) {
-            return BUZZ;
-        } else if (order % 7 == 0) {
-            return WHIZZ;
+            countOffString += FIZZ;
         }
-        return Integer.toString(order);
+        if (order % 5 == 0) {
+            countOffString += BUZZ;
+        }
+        if (order % 7 == 0) {
+            countOffString += WHIZZ;
+        }
+        return countOffString.isEmpty() ? Integer.toString(order) : countOffString;
     }
 }
