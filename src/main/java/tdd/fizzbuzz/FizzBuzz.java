f817b10dfdac92c83719563714c635685dbf38ce
Marie Chow <chowma3@oocl.com>
2022-12-02 14:21:58 +0800

    feat: implement should_say_fizz_when_count_off_given_multiple_of_3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int order) {
+        if (order % 3 == 0) {
+            return "Fizz";
+        }
         return Integer.toString(order);
     }
 }
