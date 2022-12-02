c493b681b28991709f74e3d3276394026e68958c
Marie Chow <chowma3@oocl.com>
2022-12-02 14:47:50 +0800

    feat: implement should_say_Whizz_when_count_off_given_multiple_of_7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,12 +4,15 @@ public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
 
     public String countOff(int order) {
         if (order % 3 == 0) {
             return FIZZ;
         } else if (order % 5 == 0) {
             return BUZZ;
+        } else if (order % 7 == 0) {
+            return WHIZZ;
         }
         return Integer.toString(order);
     }
