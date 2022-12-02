7e82a51446b23237d57bb47fbbed164f33ddca06
Marie Chow <chowma3@oocl.com>
2022-12-02 14:17:09 +0800

    test: add should_say_the_number_when_count_off_given_normal_number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_say_the_number_when_count_off_given_normal_number() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // when
+        String actual = fizzBuzz.countOff(1);
+        // then
+        assertEquals("1", actual);
+    }
 }
